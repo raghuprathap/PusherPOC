6a4e5c77de05d68a27d9cc2609cd5e309a75cbd8
raghu.prathap <raghu.prathap@stackroute.in>
2018-08-22 11:39:39 +0530

    Initial commit pusher POC

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('pusher-chatkit-server');
 
 const app = express()
 
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:22e27933-0947-4417-b014-33448febbc22',
+  key: 'be768a30-64f5-4dd2-9bf2-9e370761d8a4:9VIdf8LtpNZ6gwD5b2StYHMXgkDFLQDg94lbS1paSxM='
+})
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  chatkit.createUser({
+    id: username,
+    name: username
+  })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error_type === 'services/chatkit/user_already_exists') {
+        res.sendStatus(200)
+      } else {
+        res.status(error.status).json(error)
+      }
+    })
+})
+
+app.post('/authenticate', (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id })
+  res.status(authData.status).send(authData.body)
+})
 
 const PORT = 3001
 app.listen(PORT, err => {
